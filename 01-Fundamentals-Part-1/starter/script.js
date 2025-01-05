21889cbe39bf64f90a5f5deb41858128cba1ebb9
Julie Simon <sim.ju@live.fr>
2025-01-05 23:11:39 +0100

    exo start section 1 en cours

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,51 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+// Variables name conventions
+
+let myFirstJob = "Marketing manager";
+let myCurrentJob = "Developer";
+
+console.log(myCurrentJob);
+*/
+
+// fonction typeof pour connaitre le type de valeur
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+// type undefined variable vide
+let year;
+console.log(typeof year);
+
+//on assigne une valeur à la variable vide
+year = 1988;
+console.log(typeof year);
+
+// type null
+console.log(typeof null);
+// retourne object dans la console : bug
+// doit retourner null
+
+// déclarer une variable
+let age = 30;
+age = 31;
+
+const birthYear = 1988;
+
+var job = "programmer";
+job = "coder";
